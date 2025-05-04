074b056254db6403d2acd490ef2b85211f80c65c
jeon <jeonsh@apm-coin.com>
2025-05-05 00:12:05 +0900

    Update APMtoVIPBridge.sol

--- contracts/bridge/APMtoVIPBridge.sol
+++ contracts/bridge/APMtoVIPBridge.sol
@@ -2,15 +2,9 @@
 pragma solidity 0.8.20;
 
 import {IERC20} from "../openzeppelin-contracts-5.0.0/token/ERC20/IERC20.sol";
-import {VIP} from "../token/VIP.sol";
 
 contract APMToVIPBridge {
     IERC20 public immutable ERC20_APM = IERC20(0x8EB4029afd486f69e749ee172748582C2877aaAB);
-    VIP public immutable VIP_TOKEN;
-    
-    constructor(address _vip) {
-        VIP_TOKEN = VIP(_vip);
-    }
     
     /**
      * @notice IMPORTANT: Only events after 32 confirmations are considered valid for bridge processing
@@ -37,8 +31,8 @@ contract APMToVIPBridge {
         uint256 allowance = ERC20_APM.allowance(msg.sender, address(this));
         require(allowance >= amount, "Insufficient allowance");
 
-        emit Bridged(msg.sender, receiverBEP20Address, amount);
-
         require(ERC20_APM.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        
+        emit Bridged(msg.sender, receiverBEP20Address, amount);
     }
 } 
