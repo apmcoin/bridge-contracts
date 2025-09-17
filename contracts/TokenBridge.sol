b59b29425eaf391fd75a86b66c96f0db0e6b59d3
john-ver <jeonsh@apm-coin.com>
2025-09-17 23:37:08 +0900

    Refactor: Generalize bridge contract for reusability

--- contracts/TokenBridge.sol
+++ contracts/TokenBridge.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.20;
 import {IERC20} from "../openzeppelin-contracts-5.0.0/token/ERC20/IERC20.sol";
 
 contract TokenBridge {
-    IERC20 public immutable ERC20_APM;
+    IERC20 public immutable sourceToken;
     
-    constructor(address _apm) {
-        require(_apm != address(0), "Invalid APM address");
-        ERC20_APM = IERC20(_apm);
+    constructor(address _sourceToken) {
+        require(_sourceToken != address(0), "Invalid token address");
+        sourceToken = IERC20(_sourceToken);
     }
 
     /**
@@ -16,7 +16,7 @@ contract TokenBridge {
      * 
      * @param senderERC20Address    Sender address on Ethereum
      * @param receiverBEP20Address  Receiver address on BSC
-     * @param amount                Amount of APM
+     * @param amount                Amount of sourceToken
      */
     event Bridged(
         address indexed senderERC20Address,
@@ -30,13 +30,13 @@ contract TokenBridge {
     function bridge(address receiverBEP20Address) external {
         require(receiverBEP20Address != address(0), "Invalid BEP20 address");
         
-        uint256 amount = ERC20_APM.balanceOf(msg.sender);
-        require(amount > 0, "No APM balance");
+        uint256 amount = sourceToken.balanceOf(msg.sender);
+        require(amount > 0, "No token balance");
         
-        uint256 allowance = ERC20_APM.allowance(msg.sender, address(this));
+        uint256 allowance = sourceToken.allowance(msg.sender, address(this));
         require(allowance >= amount, "Insufficient allowance");
 
-        require(ERC20_APM.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(sourceToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
         
         emit Bridged(msg.sender, receiverBEP20Address, amount);
     }
